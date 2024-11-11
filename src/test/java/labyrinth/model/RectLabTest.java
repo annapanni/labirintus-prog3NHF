03bd0bdc2c9bb13a053e75b1c4091fdc989b9760
Mészáros Anna Veronika <meszaros.annapanni@gmail.com>
2024-11-11 15:53:51 +0100

    RectLabTest added

--- src/test/java/labyrinth/model/RectLabTest.java
+++ src/test/java/labyrinth/model/RectLabTest.java
@@ -5,27 +5,28 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 
-public class HexaLabTest {
-	HexaLab laby;
+public class RectLabTest {
+	RectLab laby;
 
 	@BeforeEach
 	public void setup() {
-		laby = new HexaLab(10, 8, 0.3, null);
+		laby = new RectLab(10, 8, 0.3, null);
 	}
 
 	@Test
 	public void neighbourTest() {
 		Vector midPoint = new Vector(5, 5);
 		List<Vector> all5 = laby.getAllNeighbours(midPoint);
-		assertEquals(List.of(new Vector(4, 5), new Vector(5, 4),
-			new Vector(6, 4), new Vector(6, 5),  new Vector(5, 6), new Vector(4, 6)),
+		assertEquals(List.of(new Vector(4, 5), new Vector(4, 4), new Vector(5, 4),
+			new Vector(6, 4), new Vector(6, 5), new Vector(6, 6),  new Vector(5, 6), new Vector(4, 6)),
 			all5);
 		List<Vector> valid5 = laby.getValidNeighbours(midPoint);
-		assertEquals(valid5, all5);
+		assertEquals(List.of(new Vector(4, 5), new Vector(5, 4),
+			new Vector(6, 5),  new Vector(5, 6)), valid5);
 		List<Vector> allCorner = laby.getAllNeighbours(laby.lastValidIdx());
-		assertEquals(6, allCorner.size());
+		assertEquals(8, allCorner.size());
 		List<Vector> validCorner = laby.getValidNeighbours(laby.lastValidIdx());
-		assertEquals(3, validCorner.size());
+		assertEquals(2, validCorner.size());
 		Vector parent5 = midPoint.plus(laby.getDir(midPoint));
 		assertTrue(laby.getChildren(parent5).contains(midPoint));
 	}
@@ -34,15 +35,15 @@ public class HexaLabTest {
 	public void positionTests() {
 		Vector i1 = new Vector(2, 3);
 		Vector i2 = new Vector(8, 5);
-		assertEquals(28, laby.getDist2Between(i1, i2));
+		assertEquals(40, laby.getDist2Between(i1, i2));
 		double x1 = laby.xPosition(i1);
-		assertEquals(3.5, x1);
+		assertEquals(2, x1);
 		double y1 = laby.yPosition(i1);
-		assertEquals(3 * Math.sqrt(3)/2, y1, 0.00001);
+		assertEquals(3, y1);
 		double x2 = laby.xPosition(i2);
-		assertEquals(10.5, x2);
+		assertEquals(8, x2);
 		double y2 = laby.yPosition(i2);
-		assertEquals(5 * Math.sqrt(3)/2, y2, 0.00001);
+		assertEquals(5, y2);
 		assertEquals(laby.posToVec(x1, y1), i1);
 		assertEquals(laby.posToVec(x2, y2), i2);
 	}
@@ -56,25 +57,16 @@ public class HexaLabTest {
 		assertEquals(ndir, laby.getDir(idx));
 	}
 
-	void testBoundaries() {
-		assertTrue(laby.inBound(new Vector(4, 4)));
-		assertFalse(laby.inBound(new Vector(2, 0)));
-		assertFalse(laby.inBound(new Vector(4, 7)));
-		assertFalse(laby.onBound(new Vector(4, 4)));
-		assertFalse(laby.onBound(new Vector(6, 4)));
-		assertTrue(laby.onBound(new Vector(3, 0)));
-		assertTrue(laby.onBound(new Vector(0, 3)));
-		assertTrue(laby.onBound(new Vector(9, 3)));
-		assertTrue(laby.onBound(new Vector(6, 6)));
-		assertEquals(new Vector(6, 6), laby.lastValidIdx());
-	}
-
 	@Test
 	public void boundaryTest() {
-		testBoundaries();
-		//odd height laby
-		laby = new HexaLab(10, 7, 0.3, null);
-		testBoundaries();
+		assertTrue(laby.inBound(new Vector(4, 4)));
+		assertFalse(laby.inBound(new Vector(-1, 0)));
+		assertFalse(laby.onBound(new Vector(6, 6)));
+		assertTrue(laby.onBound(new Vector(0, 4)));
+		assertTrue(laby.onBound(new Vector(9, 7)));
+		assertTrue(laby.onBound(new Vector(9, 3)));
+		assertTrue(laby.onBound(new Vector(0, 0)));
+		assertEquals(new Vector(9, 7), laby.lastValidIdx());
 	}
 
 	@Test
@@ -85,7 +77,7 @@ public class HexaLabTest {
 		double dx = poly.get(0)[0] - laby.xPosition(idx);
 		double dy = poly.get(0)[1] - laby.yPosition(idx);
 		double distFromCenter = Math.sqrt(dx*dx + dy*dy);
-		assertEquals(1 - laby.getPadding(), Math.sqrt(3) * distFromCenter, 0.00001);
+		assertEquals(1 - laby.getPadding(), Math.sqrt(2) * distFromCenter, 0.00001);
 	}
 
 
